f1129996c6f35a4641a8b719fa59f9da404122ef
sanyabanda23 <sanyabanda23@github.com>
2026-01-26 15:05:20 +0300

    1

--- .venv/app/bot/booking/windows.py
+++ .venv/app/bot/booking/windows.py
@@ -11,7 +11,8 @@ from app.bot.booking.handlers import (cancel_logic, on_phone_input, on_name_inpu
                                      process_date_end_selected, on_cost_input, on_confirmation)
 from app.bot.booking.state import BookingState
 
-def get_phone_nom_window() -> Window: 
+def get_phone_nom_window() -> Window:
+    """Окно ввода телефона гостя.""" 
     Window(
         Const("Введите номер телефона гостя."),
         MessageInput(on_phone_input),
@@ -22,7 +23,8 @@ def get_phone_nom_window() -> Window:
         state=BookingState.phone_nom
 )
 
-def get_name_window() -> Window: 
+def get_name_window() -> Window:
+    """Окно ввода имени гостя.""" 
     Window(
         Const("Введите имя гостя."),
         MessageInput(on_name_input),
@@ -34,7 +36,8 @@ def get_name_window() -> Window:
         state=BookingState.name
 )
 
-def get_user_description_window() -> Window: 
+def get_user_description_window() -> Window:
+    """Окно ввода описания гостя.""" 
     Window(
         Const("Введите краткую информацию госте."),
         MessageInput(on_description_user_input),
@@ -46,3 +49,117 @@ def get_user_description_window() -> Window:
         state=BookingState.description_user
 )
 
+def get_confirmed_old_user_window():
+    """Окно подтверждения данных старого гостя."""
+    return Window(
+        Format("{confirmed_text}"),
+        Group(
+            Button(Const("Все верно"), id="confirm1", on_click=on_confirmation_check_user_yes),
+            Button(Const("Нужно обновить информацию"), id="confirm2", on_click=on_confirmation_chek_user_no)
+            Back(Const("Назад")),
+            Cancel(Const("Отмена"), on_click=cancel_logic),
+        ),
+        state=BookingState.check_nom,
+        getter=get_confirmed_data_user
+    )
+
+def get_confirmed_new_user_window():
+    """Окно подтверждения данных нового гостя."""
+    return Window(
+        Format("{confirmed_text}"),
+        Group(
+            Button(Const("Все верно"), id="confirm1", on_click=on_confirmation_user_no),
+            Button(Const("Ввести данные заново"), id="confirm2", on_click=on_confirmation_user_no)
+            Back(Const("Назад")),
+            Cancel(Const("Отмена"), on_click=cancel_logic),
+        ),
+        state=BookingState.check_user,
+        getter=get_confirmed_data_newuser
+    )
+
+def get_table_window() -> Window:
+    """Окно выбора комнаты."""
+    return Window(
+        Format("{text_room}"),
+        ScrollingGroup(                                             # группа с возможностью пагинации для больших списков
+            Select(                                                 # специальный виджет для выбора из списка элементов.
+                Format("Комната №{item[id]} - {item[description]}"),
+                id="room_select",                                  # уникальный идентификатор виджета
+                item_id_getter=lambda items: str(items["id"]),        # функция для получения ID каждого элемента
+                items="rooms",                                     # имя ключа в данных, полученных из getter
+                on_click=on_room_selected,
+            ),
+            id="rooms_scrolling",
+            width=1,
+            height=1,
+        ),
+        Group(
+            Back(Const("Назад")),
+            Cancel(Const("Отмена"), on_click=cancel_logic),
+            width=2
+        ),
+        getter=get_all_rooms,
+        state=BookingState.room,
+    )
+
+def get_start_date_window() -> Window:
+    """Окно выбора даты заезда."""
+    return Window(
+        Const("Выбери дату заезда гостя"),
+        Calendar(
+            id="cal",
+            on_click=process_date_start_selected,
+            config=CalendarConfig(
+                firstweekday=0,                             # указал что неделя у нас начинается с понедельника
+                timezone=timezone(timedelta(hours=3)),      # установил московскую временную зону
+                min_date=date.today()                       # установил минимальную дату брони
+            )
+        ),
+        Back(Const("Назад")),
+        Cancel(Const("Отмена"), on_click=cancel_logic),
+        state=BookingState.booking_date_start,
+    )
+
+def get_end_date_window() -> Window:
+    """Окно выбора даты выезда."""
+    return Window(
+        Const("Выбери дату выезда гостя"),
+        Calendar(
+            id="cal",
+            on_click=process_date_end_selected,
+            config=CalendarConfig(
+                firstweekday=0,                             # указал что неделя у нас начинается с понедельника
+                timezone=timezone(timedelta(hours=3)),      # установил московскую временную зону
+                min_date=date.today()                       # установил минимальную дату брони
+            )
+        ),
+        Back(Const("Назад")),
+        Cancel(Const("Отмена"), on_click=cancel_logic),
+        state=BookingState.booking_date_end,
+    )
+
+def get_cost_window() -> Window:
+    """Окно ввода стоимости проживания.""" 
+    Window(
+        Const("Введите стоимость проживания за весь период."),
+        MessageInput(on_cost_input),
+        Group(
+            Back(Const("Назад")),
+            Cancel(Const("Отмена"), on_click=cancel_logic),
+            width=2
+        ),
+        state=BookingState.cost
+)
+
+def get_confirmed_new_booking_window():
+    """Окно подтверждения данных бронирования."""
+    return Window(
+        Format("{confirmed_text}"),
+        Group(
+            Button(Const("Все верно"), id="confirm", on_click=on_confirmation),
+            Back(Const("Назад")),
+            Cancel(Const("Отмена"), on_click=cancel_logic),
+        ),
+        state=BookingState.confirmation,
+        getter=get_confirmed_data_booking
+    )
